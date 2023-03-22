c9fdc629457751e6afe1e2bbea0becdb1117c273
Fadi Hania <fhania@gmail.com>
2023-03-22 08:24:03 +0200

    Add new class for Car details

---
+++ MinimalApiRouting/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalApiRouting;
+
+public class Car
+{
+    public string Id { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int ProductionYear { get; set; }
+    public string Color { get; set; }
+}
